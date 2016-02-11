9fb19137f85833efe539062c6658847a231e2d90
Thomas White <tdwhite0@gmail.com>
2016-02-11 09:43:08 -0500

    initial work on adding hero class to label

--- src/components/link/linkDirective.ts
+++ src/components/link/linkDirective.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import * as ng from 'angular';
+import {LinkTypeEnum} from './linkTypeEnum';
 
 /**
  * @ngdoc directive
@@ -23,20 +24,53 @@ import * as ng from 'angular';
  */
 export class LinkDirective implements ng.IDirective {
 
-  public restrict: string = 'E';
-  public template: string = '<a ng-href="{{ ngHref }}" class="ms-Link" ng-transclude></a>';
-  public scope: {} = {
-    ngHref: '@'
-  };
+    public restrict: string = 'E';
+    public template: string = '<a ng-href="{{ ngHref }}" class="ms-Link" ng-transclude></a>';
+    public scope: {} = {
+        ngHref: '@',
+        uifType: '@'
+    };
 
-  public transclude: boolean = true;
-  public replace: boolean = true;
+    public transclude: boolean = true;
+    public replace: boolean = true;
 
-  public static factory(): ng.IDirectiveFactory {
-    const directive: ng.IDirectiveFactory = () => new LinkDirective();
-    return directive;
-  }
+    public static factory(): ng.IDirectiveFactory {
+        const directive: ng.IDirectiveFactory = () => new LinkDirective();
+        return directive;
+    }
+
+
+    public compile(templateElement: ng.IAugmentedJQuery, templateAttributes: ng.IAttributes, transclude: ng.ITranscludeFunction)
+        : ng.IDirectivePrePost {
+
+        return {
+            pre: this.preLink
+        };
+    }
+
+    private preLink(
+        scope: any,
+        instanceElement: ng.IAugmentedJQuery,
+        attrs: any,
+        ctrls: any[],
+        transclude: ng.ITranscludeFunction): void {
+
+        scope.$watch('uifType', (newValue: string, oldValue: string) => {
+            if (LinkTypeEnum[newValue] === undefined) {
+                console.log('Error [ngOfficeUiFabric] officeuifabric.components.link - "' +
+                    newValue + '" is not a valid value for uifType. ' +
+                    'Supported options are listed here: ' +
+                    'https://github.com/ngOfficeUIFabric/ng-officeuifabric/blob/master/src/components/link/linkTypeEnum.ts');
+            }
+        });
+
+    }
 }
+
+
+
+
+
 
 /**
  * @ngdoc module
@@ -48,5 +82,5 @@ export class LinkDirective implements ng.IDirective {
  */
 export var module: ng.IModule = ng.module('officeuifabric.components.link', [
     'officeuifabric.components'
-  ])
-  .directive('uifLink', LinkDirective.factory());
+])
+    .directive('uifLink', LinkDirective.factory());
